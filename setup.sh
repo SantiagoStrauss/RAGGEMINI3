fd771f82ab6984ebc0db870e26e991145414a5e9
David Santiago Ramos Vargas <135893255+SantiagoStrauss@users.noreply.github.com>
2024-09-02 16:34:27 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@
 
 # Install spaCy model from Hugging Face
 pip install https://huggingface.co/spacy/es_core_news_sm/resolve/main/es_core_news_sm-any-py3-none-any.whl
+
+# Validate the installation of Spacy models
+python -m spacy validate
